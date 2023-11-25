207e345d8e944560de5cd60efc1641b13a400c60
Warren R. Bank <warren.r.bank@gmail.com>
2023-11-25 09:26:54 -0800

    [WIP] add implementation for methods: GM_cookie.*

---
+++ tests/0021a-set-delete-list-cookies.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         test: GM_cookie.set, GM_cookie.delete, GM_cookie.list
+// @namespace    WebViewWM
+// @match        *://*
+// @run-at       document-start
+// @grant        GM_cookie
+// ==/UserScript==
+
+var callback = console.log.bind(console)
+
+GM_cookie.delete({'name': 'foo'})
+GM_cookie.set({'name': 'foo', 'value': 'hello, foo'})
+GM_cookie.list(callback)
+
+GM_cookie.delete({'name': 'bar'})
+GM_cookie.set({'name': 'bar', 'value': 'hello, bar'})
+GM_cookie.list(callback)
+
+GM_cookie.list({'name': 'foo'}, callback)
+GM_cookie.list({'name': 'bar'}, callback)
+
+GM_cookie.delete({'name': 'foo'})
+GM_cookie.list(callback)
+
+GM_cookie.delete({'name': 'bar'})
+GM_cookie.list(callback)
---
+++ tests/0021b-set-delete-list-cookies.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         test: GM.cookie.set, GM.cookie.delete, GM.cookie.list
+// @namespace    WebViewWM
+// @match        *://*
+// @run-at       document-start
+// @grant        GM.cookie
+// ==/UserScript==
+
+var init = async function() {
+  var callback = console.log.bind(console)
+
+  await GM.cookie.delete({'name': 'foo'})
+  await GM.cookie.set({'name': 'foo', 'value': 'hello, foo'})
+  await GM.cookie.list().then(callback)
+
+  await GM.cookie.delete({'name': 'bar'})
+  await GM.cookie.set({'name': 'bar', 'value': 'hello, bar'})
+  await GM.cookie.list().then(callback)
+
+  await GM.cookie.list({'name': 'foo'}).then(callback)
+  await GM.cookie.list({'name': 'bar'}).then(callback)
+
+  await GM.cookie.delete({'name': 'foo'})
+  await GM.cookie.list().then(callback)
+
+  await GM.cookie.delete({'name': 'bar'})
+  await GM.cookie.list().then(callback)
+}
+
+init()
