warren-bank/Android-WebMonkey

commit 8a8eeb3581d7337975a6ea5e67cebfb8b6e7eb6b

detect URL navigation originating from JS by History API
* update: 'ScriptBrowser.ScriptBrowserWebViewClientGm'
* override: WebViewClient.doUpdateVisitedHistory()

summary of existing class hierarchy:
* class:   com.github.warren_bank.webmonkey.WmScriptBrowserWebViewClient_Base
  extends: at.pardus.android.webview.gm.store.ui.ScriptBrowser.ScriptBrowserWebViewClientGm
  extends: at.pardus.android.webview.gm.run.WebViewClientGm
  extends: android.webkit.WebViewClient

references:
===========
https://developer.mozilla.org/en-US/docs/Web/API/History_API
https://developer.mozilla.org/en-US/docs/Web/API/History

android-studio-project/libs/webview-gm-ui/src/main/java/at/pardus/android/webview/gm/store/ui/ScriptBrowser.java
@@ -51,6 +51,7 @@ public class ScriptBrowser {
 
   private String startUrl;
 
+  private String previousUrl;
   private String currentUrl;
 
   protected View browser;
@@ -68,20 +69,16 @@ public class ScriptBrowser {
    *            the location of the script to install
    */
   protected void installScript(String url) {
-    makeToastOnUiThread(activity.getString(R.string.starting_download_of)
-        + " " + url, Toast.LENGTH_SHORT);
+    makeToastOnUiThread(activity.getString(R.string.starting_download_of) + " " + url, Toast.LENGTH_SHORT);
     String scriptStr = DownloadHelper.downloadScript(url);
     if (scriptStr == null) {
-      makeToastOnUiThread(
-          activity.getString(R.string.error_downloading_from) + " "
-              + url, Toast.LENGTH_LONG);
+      makeToastOnUiThread(activity.getString(R.string.error_downloading_from) + " " + url, Toast.LENGTH_LONG);
       return;
     }
     Script script = Script.parse(scriptStr, url);
     if (script == null) {
       Log.d(TAG, "Error parsing script:\n" + scriptStr);
-      makeToastOnUiThread(activity.getString(R.string.error_parsing_at)
-          + " " + url, Toast.LENGTH_LONG);
+      makeToastOnUiThread(activity.getString(R.string.error_parsing_at) + " " + url, Toast.LENGTH_LONG);
       return;
     }
 
@@ -109,41 +106,44 @@ public class ScriptBrowser {
    */
   @SuppressLint("InflateParams")
   private void init() {
-    browser = activity.getLayoutInflater().inflate(
-        R.layout.script_browser, null);
+    browser = activity.getLayoutInflater().inflate(R.layout.script_browser, null);
     webView = (WebViewGm) browser.findViewById(R.id.webView);
     webView.setScriptStore(scriptStore);
     addressField = (EditText) browser.findViewById(R.id.addressField);
     addressField
         .setOnEditorActionListener(new EditText.OnEditorActionListener() {
           @Override
-          public boolean onEditorAction(TextView v, int actionId,
-              KeyEvent event) {
-            if (actionId == EditorInfo.IME_ACTION_GO
-                || actionId == EditorInfo.IME_NULL) {
-              currentUrl = v.getText().toString();
-              webView.loadUrl(currentUrl);
+          public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+            if (actionId == EditorInfo.IME_ACTION_GO || actionId == EditorInfo.IME_NULL) {
+              String url = v.getText().toString();
+              loadUrl(url);
               webView.requestFocus();
-              ((InputMethodManager) activity
-                  .getSystemService(Context.INPUT_METHOD_SERVICE))
-                  .hideSoftInputFromWindow(
-                      v.getWindowToken(), 0);
+              ((InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE))
+                  .hideSoftInputFromWindow(v.getWindowToken(), 0);
               return true;
             }
             return false;
           }
         });
-    webView.setWebViewClient(new ScriptBrowserWebViewClientGm(scriptStore,
-        webView.getWebViewClient().getJsBridgeName(), webView
-            .getWebViewClient().getSecret(), this));
+    webView.setWebViewClient(
+      new ScriptBrowserWebViewClientGm(
+        scriptStore,
+        webView.getWebViewClient().getJsBridgeName(),
+        webView.getWebViewClient().getSecret(),
+        this
+      )
+    );
     webView.setDownloadListener(new ScriptBrowserDownloadListener(this));
     webView.setWebChromeClient(new ScriptBrowserWebChromeClient(this));
     loadUrl(startUrl);
   }
 
   public void changeAddressField(String url) {
-    currentUrl = url;
-    addressField.setText(url);
+    if ((url != null) && !url.isEmpty() && !url.equals(currentUrl)) {
+      previousUrl = currentUrl;
+      currentUrl = url;
+      addressField.setText(url);
+    }
   }
 
   /**
@@ -153,25 +153,23 @@ public class ScriptBrowser {
    *            the address to load
    */
   public void loadUrl(String url) {
-    changeAddressField(url);
-    webView.loadUrl(url);
+    if ((url != null) && !url.isEmpty() && !url.equals(currentUrl)) {
+      changeAddressField(url);
+
+      webView.loadUrl(url);
+    }
   }
 
-  /**
-   * @return the browser's last loaded address
-   */
-  public String getUrl() {
-    currentUrl = webView.getUrl();
-    return currentUrl;
-  }
-
-  /**
-   * @return the browser's last loaded address
-   *
-   * Same as getUrl(), but can be called from a non-UI thread.
-   */
   public String getCurrentUrl() {
     return currentUrl;
+  }
+
+  protected String getPreviousUrl() {
+    return previousUrl;
+  }
+
+  protected void clearPreviousUrl() {
+    previousUrl = null;
   }
 
   /**
@@ -182,8 +180,7 @@ public class ScriptBrowser {
    * @param scriptStore
    *            the database to use
    */
-  public ScriptBrowser(ScriptManagerActivity activity,
-      ScriptStore scriptStore, String startUrl) {
+  public ScriptBrowser(ScriptManagerActivity activity, ScriptStore scriptStore, String startUrl) {
     this.activity = activity;
     this.scriptStore = scriptStore;
     this.startUrl = startUrl;
@@ -298,8 +295,7 @@ public class ScriptBrowser {
      * @param scriptBrowser
      *            reference to its enclosing ScriptBrowser
      */
-    public ScriptBrowserWebViewClientGm(ScriptStore scriptStore,
-        String jsBridgeName, String secret, ScriptBrowser scriptBrowser) {
+    public ScriptBrowserWebViewClientGm(ScriptStore scriptStore, String jsBridgeName, String secret, ScriptBrowser scriptBrowser) {
       super(scriptStore, jsBridgeName, secret);
       this.scriptBrowser = scriptBrowser;
     }
@@ -317,24 +313,55 @@ public class ScriptBrowser {
 
     @Override
     public void onPageStarted(WebView view, String url, Bitmap favicon) {
-      if (!ScriptBrowser.didWebViewLoadData(view)) {
-        scriptBrowser.changeAddressField(url);
-        scriptBrowser.checkDownload(url);
-      }
+      handlePageNavigation(view, url);
       super.onPageStarted(view, url, favicon);
+    }
+
+    @Override
+    public void doUpdateVisitedHistory(WebView view, String url, boolean isReload) {
+      handlePageNavigation(view, url);
+      super.doUpdateVisitedHistory(view, url, isReload);
+    }
+
+    @Override
+    public void onPageFinished(WebView view, String url) {
+      if ((url != null) && !url.isEmpty() && url.equals(scriptBrowser.getPreviousUrl())) {
+        scriptBrowser.clearPreviousUrl();
+      }
+      super.onPageFinished(view, url);
     }
 
     @SuppressWarnings("deprecation")
     @Override
-    public void onReceivedError(WebView view, int errorCode,
-        String description, String failingUrl) {
+    public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+      String text = scriptBrowser.activity.getString(R.string.error_while_loading)
+        + " "  + failingUrl
+        + ": " + errorCode
+        + " "  + description;
+
       Toast.makeText(
-          scriptBrowser.activity,
-          scriptBrowser.activity
-              .getString(R.string.error_while_loading)
-              + " "
-              + failingUrl + ": " + errorCode + " " + description,
-          Toast.LENGTH_LONG).show();
+        scriptBrowser.activity,
+        text,
+        Toast.LENGTH_LONG
+      ).show();
+    }
+
+    private void handlePageNavigation(WebView view, String url) {
+      handlePageNavigation(view, url, true, true);
+    }
+
+    private void handlePageNavigation(WebView view, String url, boolean ignoreCurrentUrl, boolean ignorePreviousUrl) {
+      if (!ScriptBrowser.didWebViewLoadData(view)) {
+        if ((url != null) && !url.isEmpty()) {
+          if (ignoreCurrentUrl && url.equals(scriptBrowser.getCurrentUrl()))
+            return;
+          if (ignorePreviousUrl && url.equals(scriptBrowser.getPreviousUrl()))
+            return;
+
+          scriptBrowser.loadUrl(url);
+          scriptBrowser.checkDownload(url);
+        }
+      }
     }
 
   }
@@ -342,8 +369,7 @@ public class ScriptBrowser {
   /**
    * DownloadListener for .user.js downloads.
    */
-  public static class ScriptBrowserDownloadListener implements
-      DownloadListener {
+  public static class ScriptBrowserDownloadListener implements DownloadListener {
 
     private ScriptBrowser scriptBrowser;
 
@@ -358,8 +384,7 @@ public class ScriptBrowser {
     }
 
     @Override
-    public void onDownloadStart(final String url, String userAgent,
-        String contentDisposition, String mimetype, long contentLength) {
+    public void onDownloadStart(final String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
       scriptBrowser.checkDownload(url);
     }
 

android-studio-project/WebMonkey/src/main/java/at/pardus/android/webview/gm/demo/WebViewGmImpl.java
@@ -128,7 +128,7 @@ public class WebViewGmImpl extends ScriptManagerActivity {
   @Override
   protected void onPause() {
     if (scriptBrowser != null) {
-      SettingsUtils.setLastUrl(this, scriptBrowser.getUrl());
+      SettingsUtils.setLastUrl(this, scriptBrowser.getCurrentUrl());
       scriptBrowser.pause();
     }
     if (scriptStore != null) {

tests/0020-history.user.js
@@ -1,0 +1,46 @@
+// ==UserScript==
+// @name         test: window.History
+// @namespace    WebViewWM
+// @match        *://*
+// @run-at       document-end
+// ==/UserScript==
+
+const urls = [
+  '/foo/',
+  '/bar/',
+  '/baz/?foo=bar',
+  '/baz/?foo=bar#baz'
+]
+
+setTimeout(
+  function() {
+    var loc = unsafeWindow.location
+    var url = loc.pathname + (loc.search || '') + (loc.hash || '')
+    var this_index = urls.indexOf(url)
+    var next_index = this_index + 1
+    var islast = (next_index === urls.length)
+
+    if (islast) {
+      unsafeWindow.history.back()
+    }
+    else {
+      url = urls[next_index]
+      if (next_index % 2 === 0)
+        unsafeWindow.history.pushState({}, '', url)
+      else
+        unsafeWindow.history.replaceState({}, '', url)
+    }
+  },
+  2500
+)
+
+const toastAlarm = setInterval(
+  function() {
+    GM_toastShort(unsafeWindow.location.href)
+  },
+  1000
+)
+
+unsafeWindow.addEventListener('beforeunload', function(event) {
+  clearInterval(toastAlarm)
+})

tests/0020-history/bar/index.html
@@ -1,0 +1,5 @@
+<html>
+<body>
+  <h1>Hello, Bar</h1>
+</body>
+</html>

tests/0020-history/baz/index.html
@@ -1,0 +1,5 @@
+<html>
+<body>
+  <h1>Hello, Baz</h1>
+</body>
+</html>

tests/0020-history/foo/index.html
@@ -1,0 +1,5 @@
+<html>
+<body>
+  <h1>Hello, Foo</h1>
+</body>
+</html>

commit c6cd10fa4fd0f8119fd34aa126ac625a685806b1

release build: v04.00.03
fix: loading URLs that contain a #hash

notes:
* v02.00.02
  - #hash worked
  - didn't include any code to override URL loading
* v03.00.00
  - #hash did not work
  - broken by commit: 8a8eeb3581d7337975a6ea5e67cebfb8b6e7eb6b
  - purpose: detect URL navigation originating from JS by History API
  - issue: #hash was (intentionally) removed

android-studio-project/constants.gradle
@@ -1,6 +1,6 @@
 project.ext {
-    releaseVersionCode  = Integer.parseInt("004000208", 10)  //Integer.MAX_VALUE == 2147483647
-    releaseVersion      = '004.00.02-08API'
+    releaseVersionCode  = Integer.parseInt("004000308", 10)  //Integer.MAX_VALUE == 2147483647
+    releaseVersion      = '004.00.03-08API'
     javaVersion         = JavaVersion.VERSION_1_8
     minSdkVersion       = 8
     targetSdkVersion    = 33

android-studio-project/libs/webview-gm-ui/src/main/java/at/pardus/android/webview/gm/store/ui/ScriptBrowser.java
@@ -55,6 +55,7 @@ public class ScriptBrowser {
   private String startUrl;
   private String currentLoadingUrl;
   private String pendingLoadingUrl;
+  private String pendingLoadingUrlWithHash;
   private String currentUrl;
 
   private Handler handler;
@@ -112,14 +113,16 @@ public class ScriptBrowser {
   private void init() {
     currentLoadingUrl = null;
     pendingLoadingUrl = null;
+    pendingLoadingUrlWithHash = null;
     currentUrl = null;
     handler = new Handler();
     loadUrlRunnable = new Runnable() {
       public void run() {
         if ((pendingLoadingUrl != null) && !pendingLoadingUrl.equals(currentLoadingUrl)&& !pendingLoadingUrl.equals(currentUrl)) {
-          loadUrlOnUiThread(webView, pendingLoadingUrl);
+          loadUrlOnUiThread(webView, pendingLoadingUrlWithHash);
         }
         pendingLoadingUrl = null;
+        pendingLoadingUrlWithHash = null;
       }
     };
     browser = activity.getLayoutInflater().inflate(R.layout.script_browser, null);
@@ -157,10 +160,12 @@ public class ScriptBrowser {
   public void changeAddressField(String url) {
     if (TextUtils.isEmpty(url)) return;
 
+    String urlWithHash = url;
+
     url = UrlUtils.removeHash(url);
     if (url.equals(currentUrl)) return;
 
-    addressField.setText(url);
+    addressField.setText(urlWithHash);
   }
 
   /**
@@ -176,6 +181,8 @@ public class ScriptBrowser {
   protected void loadUrl(String url, boolean reloadCurrentUrl) {
     if (TextUtils.isEmpty(url)) return;
 
+    String urlWithHash = url;
+
     url = UrlUtils.removeHash(url);
 
     if (url.equals(currentUrl) && reloadCurrentUrl) {
@@ -185,10 +192,11 @@ public class ScriptBrowser {
     if (url.equals(currentUrl) || url.equals(currentLoadingUrl) || url.equals(pendingLoadingUrl)) return;
 
     if ((currentUrl == null) && (currentLoadingUrl == null)) {
-      changeAddressField(url);
+      changeAddressField(urlWithHash);
     }
 
     pendingLoadingUrl = url;
+    pendingLoadingUrlWithHash = urlWithHash;
     setLoadUrlTimer();
   }
 
@@ -220,6 +228,7 @@ public class ScriptBrowser {
 
     if (UrlUtils.areEqual(currentLoadingUrl, pendingLoadingUrl)) {
       pendingLoadingUrl = null;
+      pendingLoadingUrlWithHash = null;
     }
   }
 
@@ -435,7 +444,6 @@ public class ScriptBrowser {
         url = view.getUrl();
       }
 
-      url = UrlUtils.removeHash(url);
       if (TextUtils.isEmpty(url)) return;
 
       scriptBrowser.loadUrl(url);